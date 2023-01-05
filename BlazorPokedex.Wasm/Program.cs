297c4a168a7c2731c96ca4724b38711d5206a3e8
josago97 <josago2009@hotmail.es>
2023-01-05 02:31:52 +0100

    Add prerender pages in wasm app when publish

--- BlazorPokedex.Wasm/Program.cs
+++ BlazorPokedex.Wasm/Program.cs
@@ -4,20 +4,25 @@ using BlazorPokedex.Wasm.Logic;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace BlazorPokedex.Wasm
+namespace BlazorPokedex.Wasm;
+
+public class Program
 {
-    public class Program
+    public static async Task Main(string[] args)
     {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
-            builder.RootComponents.Add<HeadOutlet>("head::after");
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddSingleton<IPokeApi, PokeApi>();
+        ConfigureServices(builder.Services, builder.HostEnvironment.BaseAddress);
 
-            await builder.Build().RunAsync();
-        }
+        await builder.Build().RunAsync();
+    }
+
+    // To able to blazor wasm prerender
+    private static void ConfigureServices(IServiceCollection services, string baseAddress)
+    {
+        services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
+        services.AddSingleton<IPokeApi, PokeApi>();
     }
 }
